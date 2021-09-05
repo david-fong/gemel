4306a116c14b815dbfd0ae23e43a7a86133fd6d7
david-fong <davidfong19@gmail.com>
2021-09-05 12:36:36 -0700

    🌷 run dart and flutter create

---
+++ packages/gemel/example/gemel_example.dart
@@ -0,0 +1,6 @@
+import 'package:gemel/gemel.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
--- src-web/client/webpack/webpack.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const CLIENT = require("./webpack.client.config");
-
-module.exports = Object.freeze({
-	client: CLIENT.CLIENT_CONFIG,
-});
